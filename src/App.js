306fcaeed027a40de5d8b780555d21bae9212405
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-03-29 18:46:26 +0530

    done boiler plate with a problem in css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
 
 function App() {
+  const [name,setName]=useState('');
+  const [list,setList]=useState([])
+  const [isEditing,setIsEditing]=useState(false);
+  const [editId,setEditId]=useState(null);
+  const [alert,setAlert]=useState({show:false , msg:'' ,type:''});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section classNam='card'>
+      <form className="grocery-form" onSubmit={handleSubmit}>
+    
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Clear items</button>
+      </div>
+    </section>
+    );
 }
 
-export default App;
+export default App
