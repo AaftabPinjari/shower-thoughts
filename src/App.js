397da20036b577b3646c278e84982a9b40bcfdce
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-03-30 21:01:52 +0530

    completed the site with room for modification will do later

--- src/App.js
+++ src/App.js
@@ -2,27 +2,113 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+import Card from 'react-bootstrap/Card'
+
+const getLocalStorage = () =>{
+  let list = localStorage.getItem('list');
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))
+  }
+  else {
+    return []
+  }
+}
+
+
 function App() {
   const [name,setName]=useState('');
-  const [list,setList]=useState([])
+  const [list,setList]=useState(getLocalStorage())
   const [isEditing,setIsEditing]=useState(false);
   const [editId,setEditId]=useState(null);
-  const [alert,setAlert]=useState({show:false , msg:'' ,type:''});
+  const [alert,setAlert]=useState(
+      {show:false , msg:'' ,type:''}
+    );
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(!name){
+      //alert
+      showAlert(true,'danger','Think of Something')
+    }
+    else if(name && isEditing){
+      //edit
+      setList(list.map((item)=>{
+        if(item.id === editId){
+          return {...item,title: name}
+        }
+        return item
+      }))
+    
+    setName('');
+    setEditId(null);
+    setIsEditing(false);
+    showAlert(true,'success','Editted')
+    }
+    else {
+      showAlert(true,'success','Brilliant Words')
+      const newItem = {id: new Date().getTime().toString(),title: name};
+      setList([newItem,...list])
+      setName("");
+    }
   }
 
+  const showAlert = (show= false ,type="",msg="") => {
+    setAlert({show,type,msg})
+  }
+
+  const clearAllItems = () => {
+    showAlert(true,'danger','All Clear')
+    setList([])
+  }
+
+  const clearPost = (id) => {
+    showAlert(true,'danger','Unspoken')
+    setList(list.filter((item)=> item.id !== id))
+  }
+
+  const editPost = (id) => {
+    const specificPost = list.find((item) => item.id == id);
+    setIsEditing(true);
+    setEditId(id)
+    setName(specificPost.title)
+  }
+
+   useEffect(()=>{
+     localStorage.setItem('list',JSON.stringify(list))
+   },[list])
+
   return (
-    <section classNam='card'>
+    <Card>
       <form className="grocery-form" onSubmit={handleSubmit}>
-    
+        {alert.show && <Alert {...alert} 
+        removeAlert={showAlert}
+        list={list} />}
+        
+        
+        <h3>Your Thoughts</h3>
+        <div className="form-control">
+          <input type="text" 
+          className=" grocery" 
+          placeholder="Speak your mind!"
+          value={name}
+          onChange={(e)=> setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+          {isEditing ? 'edit' : 'Post'}
+          </button>
+        </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">Clear items</button>
+      {list.length > 0 && (
+        <div className="grocery-container">
+        <List items={list} 
+        removePost={clearPost}
+        editPost={editPost}
+        />
+        <button className="clear-btn" onClick={clearAllItems} >Clear items</button>
       </div>
-    </section>
+      )}
+      
+    </Card>
     );
 }
 
--- src/List.js
+++ src/List.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items, removePost,editPost}) => {
+  return (
+  <div className="grocery-list">
+    {items.map((item)=>{
+      const {id,title} = item;
+      return <article key={id} className="grocery-item">
+       <p className="title">{title}</p>
+       <div className="btn-container">
+        <button type="button" 
+        className="edit-btn"
+        onClick={()=> editPost(id)}>
+          <FaEdit />
+        </button>
+        <button type="button" className="delete-btn" 
+        onClick={()=> removePost(id)}>
+          <FaTrash />
+        </button>
+       </div>
+      
+      </article>
+    })}
+  
+  </div>
+  );
 }
 
 export default List
